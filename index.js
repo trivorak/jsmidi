8de8e8f83778d21e130cdc79cd8e454a0a9c1ecc
Drake Champagne <trivorak@gmail.com>
2024-03-25 15:56:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -26,11 +26,18 @@ for (let i = 0; bufferArray.length%3;i++) {
 	bufferArray.push(0)
 }
 
+// Util Functions
+// Convert 0 - 127 to the range as defined by midi-writer-js... (0 - 100) <-for some reason
+function convertVelocity(vel){
+	return parseInt(vel/127*100)
+}
+
 // Testing adding one event. Remove after i figure out looping mech (setup as sequencer... so i don't need to calculate ticks)
 track.addEvent(new MidiWriter.NoteEvent({pitch:['E4'], duration: '4', velocity: '50'}));
 track.addEvent(new MidiWriter.NoteEvent({pitch:['c4'], duration: '4', velocity: '50'}));
 
-// Outputs Midifile in working directory... it's ugly but it works
-const write = new MidiWriter.Writer(track);
-const midiData = write.dataUri().split(';base64,').pop();
-fs.writeFileSync('test.midi',midiData,{encoding:'base64'});
+// Uncomment Below to get export functions back///////////////////////////
+// // Outputs Midifile in working directory... it's ugly but it works
+// const write = new MidiWriter.Writer(track);
+// const midiData = write.dataUri().split(';base64,').pop();
+// fs.writeFileSync('test.midi',midiData,{encoding:'base64'});
