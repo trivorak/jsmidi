321e29f2a1051b54f47a979bb9460606453fcbe4
Drake Champagne <trivorak@gmail.com>
2024-03-25 15:31:34 -0500

    update main / ignore file
    Ignoring .midi files and updated index.js to include exporting

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 // Must Know 128 ticks per beat //
 
 const fs = require('node:fs');
+const MidiWriter = require('midi-writer-js');
+const track = new MidiWriter.Track();
 
 let = bufferArray = [];
 
@@ -19,4 +21,12 @@ try {
 // Transfer array to a range of 0 - 127 for midi 
 bufferArray = Array.from(bufferArray,(x) => parseInt(x / 2));
 
-console.log(bufferArray)
+
+// Testing adding one event. Remove after i figure out looping mech (setup as sequencer... so i don't need to calculate ticks)
+track.addEvent(new MidiWriter.NoteEvent({pitch:['E4'], duration: '4', velocity: '50'}));
+track.addEvent(new MidiWriter.NoteEvent({pitch:['c4'], duration: '4', velocity: '50'}));
+
+// Outputs Midifile in working directory... it's ugly but it works
+const write = new MidiWriter.Writer(track);
+const midiData = write.dataUri().split(';base64,').pop();
+fs.writeFileSync('test.midi',midiData,{encoding:'base64'});
