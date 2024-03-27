37e0a139de516e67391471d267cd85b14ed0216b
Drake Champagne <trivorak@gmail.com>
2024-03-26 23:18:16 -0500

    added scales.js file. basically copied off of my python script

---
+++ scales.js
@@ -0,0 +1,27 @@
+const majorScale = [0,2,4,5,7,9,11];
+const naturalMinorScale = [0,2,3,5,7,8,10];
+const harmonicMinorScale = [0,2,3,5,7,8,11];
+const melodicMinorScale = [0,2,3,5,7,9,11];
+const dorianScale = [0,2,3,5,7,9,10];
+const locrianScale = [0,1,3,4,7,8,10];
+const lydianScale = [0,2,4,6,7,9,11];
+const mixolydianScale = [0,2,4,5,7,9,10];
+const phrygianScale =[0,1,3,5,7,8,10];
+const pentatonicMajorScale = [0,2,4,7,9];
+const pentatonicMinorScale = [0,3,5,7,10];
+
+const scales = [majorScale,naturalMinorScale,harmonicMinorScale,
+        melodicMinorScale,dorianScale,locrianScale,lydianScale,
+        mixolydianScale,phrygianScale,pentatonicMajorScale,pentatonicMinorScale];
+
+function getAllScales(){
+    return scales;
+};
+
+function getScaleCount(){
+    return scales.length;
+};
+
+function getScale(array, i){
+    return array[i];
+};
