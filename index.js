647bb34ab295c153c7370b15a0add88b78dc2b70
Drake Champagne <trivorak@gmail.com>
2024-04-01 23:30:37 -0500

    pass argument for length

--- index.js
+++ index.js
@@ -1,115 +1,119 @@
 // As Defined by Library //
 // Must Know 128 ticks per beat //
 
-import fs from 'fs';
-import MidiWriter from 'midi-writer-js';
-import { convertNotes , getNote , getNoteDuration } from './notes.js';
-import { getScaleCount , getScale , getScaleArray, getScaleName} from './scales.js'
-import { argv } from 'node:process';
+import fs from "fs";
+import MidiWriter from "midi-writer-js";
+import { convertNotes, getNote, getNoteDuration } from "./notes.js";
+import {
+  getScaleCount,
+  getScale,
+  getScaleArray,
+  getScaleName,
+} from "./scales.js";
+import { argv } from "node:process";
 
-const chordNoteCount = 4;
+// const chordNoteCount = 4;
 
-// Input 
+// Input
 let inputFile;
-if (argv.length > 2){
-	inputFile = argv[2]
-}else{
-	console.log("Please pass an input file as an argument")
-	process.exit()
+if (argv.length > 2) {
+  inputFile = argv[2];
+} else {
+  console.log("Please pass an input file as an argument");
+  process.exit();
 }
 
-console.log(`Input File = ${inputFile}\n`)
+let chordNoteCount;
+if (argv.length > 3) {
+  chordNoteCount = parseInt(argv[3]);
+} else {
+  chordNoteCount = 4;
+}
+
+console.log(`Input File = ${inputFile}\n`);
 const track = new MidiWriter.Track();
 
 let bufferArray = [];
 
 // Open File and store array into bufferArray
 try {
-	const data = fs.readFileSync(inputFile);
+  const data = fs.readFileSync(inputFile);
 
-	for (let i = 0; i < data.length; i++){
-		bufferArray.push(data[i]);
-	}
-} catch(err) {
-	console.error(err);
+  for (let i = 0; i < data.length; i++) {
+    bufferArray.push(data[i]);
+  }
+} catch (err) {
+  console.error(err);
 }
 
 // Store length of array before adjustments
-const bufferLength = bufferArray.length
+const bufferLength = bufferArray.length;
 
-
-// Transfer array to a range of 0 - 127 for midi 
+// Transfer array to a range of 0 - 127 for midi
 // Trim data to the first 50K elements for "less runtimes"
 // Log out Trimmed Size
 bufferArray = bufferArray.map((x) => parseInt(x / 2));
-bufferArray = bufferArray.slice(0,50000);
-console.log(`Trimmed Buffer Length: ${bufferArray.length}`)
+bufferArray = bufferArray.slice(0, 50000);
+console.log(`Trimmed Buffer Length: ${bufferArray.length}`);
 
 // Fix length of array. Must be divisible by Chord Count(+Velocity,+Duration) . (Note,Velocity,Duration)
-for (let i = 0; bufferArray.length % (chordNoteCount + 2) ; i++) {
-	bufferArray.push(0);
+for (let i = 0; bufferArray.length % (chordNoteCount + 2); i++) {
+  bufferArray.push(0);
 }
-
 
 // Main Scale / Key Variables
 const rootNote = bufferLength % 12;
-const scaleSelection = parseInt(bufferLength / getScaleCount()) % getScaleCount();
-const scaleArray = getScaleArray(rootNote,getScale(scaleSelection));
+const scaleSelection =
+  parseInt(bufferLength / getScaleCount()) % getScaleCount();
+const scaleArray = getScaleArray(rootNote, getScale(scaleSelection));
 const hiNote = Math.max(...scaleArray);
 const loNote = Math.min(...scaleArray);
 
-
 // Util Functions
 // Convert 0 - 127 to the range as defined by midi-writer-js... (0 - 100) <-for some reason
-function convertVelocity(vel){
-	return parseInt(vel / 127 * 100);
+function convertVelocity(vel) {
+  return parseInt((vel / 127) * 100);
 }
 
-function writeMidi(data){
-	track.addEvent(new MidiWriter.NoteEvent(data));
+function writeMidi(data) {
+  track.addEvent(new MidiWriter.NoteEvent(data));
 }
 
-function snapToScale(note){
-	const hiNote = Math.max(...scaleArray);
+function snapToScale(note) {
+  const hiNote = Math.max(...scaleArray);
 
-	for (let i = 0; i < scaleArray.length; i++){
-		if (scaleArray[i] > note){
-			return scaleArray[i-1]
-			break
-		}
-		else if (scaleArray[i] === note){
-			return scaleArray[i]
-			break
-		}
-		else if (note > hiNote) {
-			return hiNote
-		}
-	}
+  for (let i = 0; i < scaleArray.length; i++) {
+    if (scaleArray[i] > note) {
+      return scaleArray[i - 1];
+      break;
+    } else if (scaleArray[i] === note) {
+      return scaleArray[i];
+      break;
+    } else if (note > hiNote) {
+      return hiNote;
+    }
+  }
 }
-
 
 // Main forloop to add notes to the midi track
-for (let i = 0; i<bufferArray.length;i+=chordNoteCount+2){
-
-	let notesArray = bufferArray.slice(i,i+chordNoteCount-1);
-	notesArray = notesArray.map((x) =>convertNotes(snapToScale(x)))
-
-	let datapacket = {
-		pitch: notesArray,
-		duration: getNoteDuration(bufferArray[i+chordNoteCount]),
-		velocity: convertVelocity(bufferArray[i+chordNoteCount+1]),
-	}
-	writeMidi(datapacket);
+for (let i = 0; i < bufferArray.length; i += chordNoteCount + 2) {
+  let notesArray = bufferArray.slice(i, i + chordNoteCount - 1);
+  notesArray = notesArray.map((x) => convertNotes(snapToScale(x)));
+  console.log(bufferArray[i + chordNoteCount]);
+  let datapacket = {
+    pitch: notesArray,
+    duration: getNoteDuration(bufferArray[i + chordNoteCount]),
+    velocity: convertVelocity(bufferArray[i + chordNoteCount + 1]),
+  };
+  writeMidi(datapacket);
 }
-
 
 // Print Root note
 console.log(`Root Note: ${getNote(rootNote)}`);
 console.log(`Scale : ${getScaleName(scaleSelection)}`);
 
-
 // Outputs Midifile in working directory... it's ugly but it works
 const write = new MidiWriter.Writer(track);
-const midiData = write.dataUri().split(';base64,').pop();
-fs.writeFileSync('test.midi',midiData,{encoding:'base64'});
+const midiData = write.dataUri().split(";base64,").pop();
+fs.writeFileSync("test.midi", midiData, { encoding: "base64" });
 console.log(`Complete\n`);
--- notes.js
+++ notes.js
@@ -1,107 +1,110 @@
 const notes = [
-    {
-        id: 0,
-        name: 'C'
-    },
-    {
-        id: 1,
-        name: 'C#'
-    },
-    {
-        id: 2,
-        name: 'D'
-    },
-    {
-        id: 3,
-        name: 'D#'
-    },
-    {
-        id: 4,
-        name: 'E'
-    },
-    {
-        id: 5,
-        name: 'F'
-    },
-    {
-        id: 6,
-        name: 'F#'
-    },
-    {
-        id: 7,
-        name: 'G'
-    },
-    {
-        id: 8,
-        name: 'G#'
-    },
-    {
-        id: 9,
-        name: 'A'
-    },
-    {
-        id: 10,
-        name: 'A#'
-    },
-    {
-        id: 11,
-        name: 'B'
-    }
+  {
+    id: 0,
+    name: "C",
+  },
+  {
+    id: 1,
+    name: "C#",
+  },
+  {
+    id: 2,
+    name: "D",
+  },
+  {
+    id: 3,
+    name: "D#",
+  },
+  {
+    id: 4,
+    name: "E",
+  },
+  {
+    id: 5,
+    name: "F",
+  },
+  {
+    id: 6,
+    name: "F#",
+  },
+  {
+    id: 7,
+    name: "G",
+  },
+  {
+    id: 8,
+    name: "G#",
+  },
+  {
+    id: 9,
+    name: "A",
+  },
+  {
+    id: 10,
+    name: "A#",
+  },
+  {
+    id: 11,
+    name: "B",
+  },
 ];
 
 const noteDuration = [
-    {
-        id: 0,
-        duration: 16
-    },
-    {
-        id: 1,
-        duration: 8
-    },
-    {
-        id: 2,
-        duration: 4
-    },
-    {
-        id: 3,
-        duration: 2
-    },
-    {
-        id: 4,
-        duration: 1
-    }
-
-]
+  {
+    id: 0,
+    duration: 16,
+  },
+  {
+    id: 1,
+    duration: 8,
+  },
+  {
+    id: 2,
+    duration: 4,
+  },
+  {
+    id: 3,
+    duration: 2,
+  },
+  {
+    id: 4,
+    duration: 1,
+  },
+];
 
 // Lookup note names from larger list
-export function getNote(input){
-    if (isNaN(input)){
-        input = 0
-    }
-    let notenumber = input % 12;
-    try{
-        return notes.find(x => x.id === notenumber).name;
-    }catch (error){
-        console.error(error)
-    }
-}   
-
+export function getNote(input) {
+  if (isNaN(input)) {
+    input = 0;
+  }
+  let notenumber = input % 12;
+  try {
+    return notes.find((x) => x.id === notenumber).name;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 // Return Octave of Note
-function getOctave(input){
-    return parseInt((input)/12)-1;
+function getOctave(input) {
+  return parseInt(input / 12) - 1;
 }
 
 // Larger function bringing everything together
-export function convertNotes(input){
-    let noteName = getNote(input);
-    let octNumber = getOctave(input);
-    return noteName+String(octNumber);
+export function convertNotes(input) {
+  let noteName = getNote(input);
+  let octNumber = getOctave(input);
+  return noteName + String(octNumber);
 }
 
-// return the note duration based on the noteDuration const json. 
-export function getNoteDuration(input){
-    let inputLength = Math.round((input / 127) * (noteDuration.length - 1))
-    return noteDuration.find(x => x.id === inputLength).duration
-}
+// return the note duration based on the noteDuration const json.
+export function getNoteDuration(input) {
+  let inputLength = Math.round((input / 127) * (noteDuration.length - 1));
 
+  if (inputLength < 0) {
+    inputLength = 0;
+  }
+
+  console.log(inputLength);
+  return noteDuration.find((x) => x.id === inputLength).duration;
+}
